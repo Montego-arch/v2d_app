3a7e1973eeb0131af5a5949419a692ff4b0565ae
Montego-arch <mmanuelmiles@gmail.com>
2024-03-22 13:41:04 +0100

    made customer area of interest unmandatory

---
+++ v2d_app/v2d_app/doctype/field_sales/field_sales.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, montego and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class fieldsales(Document):
+	pass
